8176d2ca81f7bbf1cfcd3890436c66d25b36d5ba
Michael Bricout <bricout.michael@gmail.com>
2025-01-08 08:28:39 +0100

    Exercice 9 complété

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,16 @@
 // votre code ici
-
+function checkAge(age)
+{
+    if (age < 18)
+    {
+        alert("vous êtes mineur");
+    }
+    else if(age >= 18 && age <= 65)
+    {
+        alert("Vous êtes majeur");
+    }
+    else {
+        alert("vous êtes senior");
+    }
+}
 export default checkAge;
