1a49eda9d305a15b47b198acc7e495f04fcfa43e
Michael Bricout <bricout.michael@gmail.com>
2025-01-08 10:43:41 +0100

    Fix du parseFloat sur Exercice 8

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,7 +1,10 @@
 //Votre code ici
 function add(numb1, numb2) {
-   if (typeof numb1 !== 'number' || typeof numb2 !== 'number') {
-        alert("Ce ne sont pas des int ou float")
+  if (typeof numb1 !== 'number' || typeof numb2 !== 'number') {
+        //return "Ce ne sont pas des int ou float";
+        numb1 = parseFloat(numb1);
+        numb2 = parseFloat(numb2);
+        return numb1 + numb2;
     }
     else {
         return numb1 + numb2;
