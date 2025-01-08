2d089d330af7cbfa3def6ba2c4516e8b65303e2c
Michael Bricout <bricout.michael@gmail.com>
2025-01-08 08:18:29 +0100

    Exercice 8 complété

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,10 @@
 //Votre code ici
-
+function add(numb1, numb2) {
+    if (numb1 !== 'number' || numb2 !== 'number') {
+        alert("Ce ne sont pas des int ou float")
+    }
+    else {
+        return numb1 + numb2;
+    }
+}
  export default add;
