fa9239c2b90860a735a2b518e5db261da61c0148
Michael Bricout <bricout.michael@gmail.com>
2025-01-08 09:49:35 +0100

    Exercice 10 complété

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,12 @@
 //votre code ici
-
-export default printNumbers
+function printNumbers(n)
+{
+let numbers = [];
+for (let i = 0; i < n; i++) {
+    numbers.push(i);
+}
+console.log(numbers.join(" "));
+alert(numbers.join(" "));
+}
+//printNumbers(10)
+export default printNumbers;
